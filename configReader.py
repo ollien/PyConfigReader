bf18c698596be9de094d94cdc52d95186fc37e6a
Nick Krichevsky <njk828@gmail.com>
2014-11-27 22:20:02 -0500

    Add mode for changing what file it is, and fix a bug where a line without an equals wouldn't work

--- configReader.py
+++ configReader.py
@@ -1,10 +1,10 @@
 class ConfigReader():
-	def __init__(self):
+	def __init__(self,name="config.txt"):
 		self.keys={}
-		
+		self.name = name
 	#Read Keys from file
 	def readKeys(self):
-		keysFile=open("config.txt","r")
+		keysFile=open(self.name,"r")
 		fileLines=keysFile.readlines()
 		keysFile.close()
 		self.keys.clear()
@@ -20,13 +20,14 @@ class ConfigReader():
 				continue
 			else:
 				#Get Position of equal sign
-				pos=item.index('=')
-				#Name of the key is [0:pos], Value of the key is [pos+1:-1] (Stripping the \n char at the end)
-				self.keys[item[0:pos]]=item[pos+1:]
+				pos=item.find('=')
+				if pos != -1:
+					#Name of the key is [0:pos], Value of the key is [pos+1:-1] (Stripping the \n char at the end)
+					self.keys[item[0:pos]]=item[pos+1:]
 					
 	#Return the keys, read allows you to get the keys without re-reading the file.
 	def getKeys(self,read=True):
 		if read:
 			self.readKeys()
-		return self.keysFile
+		return self.keys
 			
